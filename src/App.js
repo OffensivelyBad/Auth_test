b5c9c82d14e65eea4c42fb656faba177c6ca439a
SRoller <sroller@justfab.com>
2018-01-21 18:38:56 -0500

    adds firebase config

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: '',
+      authDomain: 'babymonitor-1bed4.firebaseapp.com',
+      databaseURL: 'https://babymonitor-1bed4.firebaseio.com',
+      projectId: 'babymonitor-1bed4',
+      storageBucket: 'babymonitor-1bed4.appspot.com',
+      messagingSenderId: '829122627260'
+    });
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Auth" />
-        <Text>testes123</Text>
+        <LoginForm />
       </View>
     );
   }
