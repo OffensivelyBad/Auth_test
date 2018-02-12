cecd200c7adc84ba07a6b6597584dc1f8b57c5c9
SRoller <sroller@justfab.com>
2018-02-12 17:46:52 -0500

    adds input components

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,16 +1,44 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import firebase from 'firebase';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
+    state = { 
+        email: '',
+        password: ''
+    };
+
+    onButtonPress() {
+        const { email, password } = this.state;
+
+        firebase.auth().signInWithEmailAndPassword(email, password);
+    }
+
     render() {
         return (
             <Card>
-                <CardSection />
-                <CardSection />
+                <CardSection>
+                    <Input 
+                        label="Email"
+                        placeholder="user@email.com"
+                        autoCapitalize={"none"}
+                        value={this.state.email}
+                        onChangeText={text => this.setState({ email: text })}
+                    />
+                </CardSection>
+                <CardSection>
+                    <Input 
+                        label="Password"
+                        placeholder="password"
+                        autoCapitalize={"none"}
+                        secureTextEntry
+                        value={this.state.password}
+                        onChangeText={text => this.setState({ password: text })}
+                    />
+                </CardSection>
 
                 <CardSection>
-                    <Button>
+                    <Button onPress={this.onButtonPress.bind(this)}>
                         Log in
                     </Button>
                 </CardSection>
